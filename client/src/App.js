3d954aebaaded9f2cda3947f344e35cb7c807164
진현기 <jinhyunki@jinhyeongiui-MacBookPro.local>
2020-06-26 18:05:20 +0900

    Add get customers Api Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,36 +18,26 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
-
-const customers = [
-{
-  'id': 1,
-  'image': 'https://placeimage.com/64/64/1',
-  'name': '진예서',
-  'birthday': '091231',
-  'gender': '여자',
-  'job': '첫째'
-},
-{
-  'id': 2,
-  'image': 'https://placeimage.com/64/64/2',
-  'name': '진예림',
-  'birthday': '140530',
-  'gender': '여자',
-  'job': '둘째'
-},
-{
-  'id': 3,
-  'image': 'https://placeimage.com/64/64/3',
-  'name': '진예림',
-  'birthday': '160428',
-  'gender': '남자',
-  'job': '셋째'
-}
-]
+});
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err)); 
+  }
+
+  callApi = async () => {
+    const response = await fetch('api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const { classes } =this.props;
     return(
@@ -63,7 +53,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {return(<Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+            {this.state.customers? this.state.customers.map(c => {return(<Customer key={c.id} id={c.id} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)}): ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req,res) => {
-  res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimage.com/64/64/1',
+      'name': '진예서',
+      'birthday': '091231',
+      'gender': '여자',
+      'job': '첫째'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimage.com/64/64/2',
+      'name': '진예림',
+      'birthday': '140530',
+      'gender': '여자',
+      'job': '둘째'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimage.com/64/64/3',
+      'name': '진예림',
+      'birthday': '160428',
+      'gender': '남자',
+      'job': '셋째'
+    }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
